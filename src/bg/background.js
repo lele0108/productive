abbdedd9e730741c7b84a2862dc3d08c8a1982d4
Jimmy Liu <sicong.liu98@gmail.com>
2014-07-22 20:29:41 -0700

    commit

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,9 +6,23 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+var apikey;
 
+$(window).load(function() 
+{
+   // executes when complete page is fully loaded, including all frames, objects and images
+   $.getJSON("https://todoist.com/API/login?password=lele@6871&email=sicong.liu98@gmail.com&callback=?", function(data) {
+    	// Get the element with id summary and set the inner text to the result.
+    	apikey = data.api_token;
+    	getp();
+	});
+});
+
+function getp() {
+	var url = "https://todoist.com/API/getProductivityStats?token=" + apikey + "&callback=?";
+	$.getJSON(url, function(data) {
+    	// Get the element with id summary and set the inner text to the result.
+    	var test = data.days_items[0].total_completed;
+    	alert(test);
+	});
+}
