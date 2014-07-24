c0c9ac3799c2e40a6413cb2e3f1854ac125fb54e
Jimmy Liu <sicong.liu98@gmail.com>
2014-07-24 00:33:52 -0700

    alarms working

--- src/bg/eventPage.js
+++ src/bg/eventPage.js
@@ -1,3 +1,12 @@
 chrome.alarms.onAlarm.addListener(function(alarm) {
-  alert("Beep");
+  var url = "https://todoist.com/API/getProductivityStats?token=" + localStorage.getItem('apikey') + "&callback=?";
+	$.getJSON(url, function(data) {
+    	// Get the element with id summary and set the inner text to the result.
+      var tasks = data.days_items[0].total_completed;
+      var time = tasks * 20;
+      console.log(time);
+      localStorage.setItem('tasks',tasks);
+      localStorage.setItem('time', time);
+      alert(localStorage.getItem('time'));
+	});
 });
