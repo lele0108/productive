f7ac0450f4803662caa51fcf154cc6324c621621
Jimmy Liu <sicong.liu98@gmail.com>
2014-07-23 23:37:27 -0700

    todoist login

--- src/bg/background.js
+++ src/bg/background.js
@@ -10,19 +10,33 @@ var apikey;
 
 $(window).load(function() 
 {
-   // executes when complete page is fully loaded, including all frames, objects and images
-   $.getJSON("https://todoist.com/API/login?password=lele@6871&email=sicong.liu98@gmail.com&callback=?", function(data) {
-    	// Get the element with id summary and set the inner text to the result.
-    	apikey = data.api_token;
-    	getp();
-	});
+  $( ".go" ).click(function() {
+    var usr = $('.email').val();
+    var pswd = $('.pswd').val();
+    getapikey(usr, pswd);
+  });
 });
+
+function getapikey(username, password) {
+  var url = "https://todoist.com/API/login?password=" + password + "&email=" + username + "&callback=?";
+  $.getJSON(url, function(data) {
+      if (data == "LOGIN_ERROR") {
+        $( ".info" ).append('<p style="color: red;">try again</p>');
+        return;
+      }
+      apikey = data.api_token;
+      localStorage.setItem('apikey',apikey);
+      $( ".info" ).append('<p style="color: green;">login success</p>');
+      getp();
+  });
+}
 
 function getp() {
 	var url = "https://todoist.com/API/getProductivityStats?token=" + apikey + "&callback=?";
 	$.getJSON(url, function(data) {
     	// Get the element with id summary and set the inner text to the result.
     	var test = data.days_items[0].total_completed;
-    	alert(test);
+      localStorage.setItem('score',test);
+    	alert(localStorage.getItem('score'));
 	});
 }
